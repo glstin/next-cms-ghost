69c3f4afafd3c2cfc23ce7ad1dea4aaa1682abb8
Gael Mugeni <gael.justin@gmail.com>
2021-05-25 00:21:46 +0200

    Create faq.tsx

---
+++ pages/faq.tsx
@@ -0,0 +1,52 @@
+import Link from 'next/link'
+import { GetStaticProps } from 'next'
+
+import { Layout } from '@components/Layout'
+import { HeaderPage } from '@components/HeaderPage'
+import { PostCard } from '@components/PostCard'
+
+import { getAllPosts, getAllSettings, GhostSettings, GhostPostsOrPages } from '@lib/ghost'
+import { useLang, get } from '@utils/use-lang'
+import { BodyClass } from '@helpers/BodyClass'
+
+export const getStaticProps: GetStaticProps = async () => {
+  const posts = await getAllPosts({ limit: 3 })
+  const settings = await getAllSettings()
+
+  return {
+    props: {
+      settings,
+      posts,
+      bodyClass: BodyClass({})
+    },
+  }
+}
+
+interface Custom404Props {
+  posts: GhostPostsOrPages
+  settings: GhostSettings
+  bodyClass: string
+}
+
+export default function Custom404({ posts, settings, bodyClass }: Custom404Props) {
+  const text = get(useLang())
+
+  return (
+    <Layout {...{ settings, bodyClass }} header={<HeaderPage {...{ settings }} />} errorClass="error-content">
+      <div className="inner">
+        <section className="faq">
+          <h1 className="faq">FAQ</h1>
+          <p className="error-description">{text(`FAQ`)}</p>
+          <Link href="/" ><a className="error-link">{text(`GOTO_FRONT_PAGE`)} →</a></Link>
+        </section>
+
+        <div className="post-feed">
+          {posts.map((post, i) => (
+            <PostCard key={post.id} {...{ settings, post, num: i }} />
+          ))}
+        </div>
+
+      </div>
+    </Layout>
+  )
+}
