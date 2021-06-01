db5523fde941d20de216c9eb4d8036322c1ed31f
Gael Mugeni <gael.justin@gmail.com>
2021-06-01 19:54:18 +0200

    Create Submit.ts

---
+++ pages/api/Submit.ts
@@ -0,0 +1,37 @@
+import Link from 'next/link'
+import { GetStaticProps } from 'next'
+
+import { Layout } from '@components/Layout'
+import { HeaderPage } from '@components/HeaderPage'
+import { PostCard } from '@components/PostCard'
+
+import { getAllPosts, getAllSettings, GhostSettings, GhostPostsOrPages } from '@lib/ghost'
+import { useLang, get } from '@utils/use-lang'
+import { BodyClass } from '@helpers/BodyClass'
+
+export const getStaticProps: GetStaticProps = async () => {
+  const posts = await getAllPosts({ limit: 3 })
+  const settings = await getAllSettings()
+
+  return {
+    props: {
+      settings,
+      posts,
+      bodyClass: BodyClass({})
+    },
+  }
+}
+
+interface Custom404Props {
+  posts: GhostPostsOrPages
+  settings: GhostSettings
+  bodyClass: string
+}
+
+export default function Custom404({ posts, settings, bodyClass }: Custom404Props) {
+  const text = get(useLang())
+
+  return (
+   
+  )
+}
